f76e281c516e1aeac934354cd0ce6dc98ce2b64d
Lucas Brito <britolmbs@hotmail.com>
2025-03-09 07:27:53 -0300

    Inicinado o projeto

---
+++ tocadosbichos/src/app/_componets/hero.tsx
@@ -0,0 +1,7 @@
+export function Hero() {
+    return(
+        <section className="bg-[#E84C3D] text-white relative overflow-hidden">
+         
+        </section>
+    )
+}
---
+++ tocadosbichos/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "./_componets/hero";
+
+export default function Home() {
+  return(
+    <main>
+      <Hero />
+    </main>
+  )
+}
