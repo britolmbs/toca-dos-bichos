b9ed338cd88a684c63f3d41b9dda057740399d9e
Lucas Brito <britolmbs@hotmail.com>
2025-03-25 21:26:55 -0300

    Adicionando grid responsivo para exibir marcas

--- tocadosbichos/src/app/_componets/footer.tsx
+++ tocadosbichos/src/app/_componets/footer.tsx
@@ -3,6 +3,7 @@ import golden from '../../../public/golden.png'
 import premier from '../../../public/primier.png'
 import natural from '../../../public/natural.png'
 import whiskas from '../../../public/whiskas.png'
+import Image from 'next/image'
 
 const brands =[
     { name: "Royal Canin", logo: royal },
@@ -17,6 +18,25 @@ export function Footer(){
             <div className='container mx-auto px-4'>
                 <div className="border-b border-white/20 pb-8">
                 <h4 className='text-3xl font-semibold mb-8 text-center'>Marcas que trabalhamos</h4>
+
+                <div className='grid grid-cols-2 lg:grid-cols-6 gap-8'>
+                    {brands.map((item, index) => (
+                        <div key={index} className='bg-white p-4 rounded-lg flex items-center justify-center'>
+                            <Image 
+                            src={item.logo}
+                            alt={item.name} 
+                            className='object-contain'
+                            width={100}
+                            height={50}
+                            quality={100}
+                            style={{
+                                 width: 'auto',
+                                height: 'auto',
+                                    }} />
+                            
+                        </div>
+                    ))}
+                </div>
                 </div>
             </div>
 
